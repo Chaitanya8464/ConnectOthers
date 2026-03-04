b23737a2509b9ebae02922e4108e5c0a70f42e26
Chaitanya Verma <chaitanyaa.vemaa@gmail.com>
2026-03-04 20:22:08 +0530

    Index is created

---
+++ frontend-v2/src/components/chat/index.js
@@ -0,0 +1,17 @@
+export { ConversationList } from './ConversationList';
+export { MessageList } from './MessageList';
+export { MessageInput } from './MessageInput';
+export { ChatHeader } from './ChatHeader';
+export { ChatTabs } from './ChatTabs';
+export { NewChatModal } from './NewChatModal';
+export { NewGroupModal } from './NewGroupModal';
+export { AIAssistantButton } from './AIAssistantButton';
+export { AutoReplySuggestions } from './AutoReplySuggestions';
+export { ConversationAnalysisModal } from './ConversationAnalysisModal';
+export { FilePermissionModal } from './FilePermissionModal';
+export { EmailPreviewModal } from './EmailPreviewModal';
+export { SettingsModal } from './SettingsModal';
+export { ProfileModal } from './ProfileModal';
+export { CalendarPreviewModal } from './CalendarPreviewModal';
+export { BrowseGroupsModal } from './BrowseGroupsModal';
+export { OnlineUsersPanel } from './OnlineUsersPanel';
